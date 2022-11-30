92f54000d2178e7520203cc2b2865b779a13b2b5
Bobby Connolly <bobbyconnolly@bobbys-mbp.lan>
2022-11-29 20:54:43 -0500

    happy path

--- lecture-10-fetch-dog-images/script.js
+++ lecture-10-fetch-dog-images/script.js
@@ -1,32 +1,104 @@
-function getDogImage() {
+function getCeoDogImage() {
     return fetch("https://dog.ceo/api/breeds/image/random")
         .then((response) => {
             return response.json()
         })
         .then((data) => {
+            // console.log("smokey", data)
             return data.message
         })
 }
 
-function getWoofImage() {
+function getWoofDogImage() {
     return fetch("https://random.dog/woof.json")
         .then((response) => {
             return response.json()
         })
         .then((data) => {
+            // console.log("thumbelina", data)
             return data.url
         })
 }
 
-function getPairOfPuppyImageUrlsAndUpdateDOM() {
+function getPairOfPuppiesAndUpdateDOM() {
     document.getElementById("fetch-puppies-btn").disabled = true
 
-    Promise.all([getDogImage(), getWoofImage()]).then((data) => {
-        const [image1Url, image2Url] = data
+    return Promise.all([getCeoDogImage(), getWoofDogImage()]).then((data) => {
+        const [ceoDogUrl, woofDogUrl] = data
 
-        document.getElementById("image-1").src = image1Url
-        document.getElementById("image-2").src = image2Url
+        document.getElementById("image-ceo").src = ceoDogUrl
+        document.getElementById("image-woof").src = woofDogUrl
     })
 }
 
-// getPairOfPuppyImageUrlsAndUpdateDOM()
+const state = {
+    scoreLeft: 0,
+    scoreRight: 0,
+    resolve: null,
+}
+
+// wait for dom content to load
+
+document.addEventListener("DOMContentLoaded", () => {
+    // This is the start of our program (the entry point)
+
+    const scoreLeft = document.getElementById("score-left")
+    const scoreRight = document.getElementById("score-right")
+
+    setInterval(() => {
+        scoreLeft.innerText = state.scoreLeft
+        scoreRight.innerText = state.scoreRight
+    }, 200)
+
+    roundOfCompetition()
+})
+
+function roundOfCompetition() {
+    const modal = document.getElementById("modal-container")
+
+    getPairOfPuppiesAndUpdateDOM()
+        .then(() => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve()
+                }, 2000)
+            })
+        })
+        .then(() => {
+            modal.style.display = "block"
+
+            // We need to WAIT until the user makes a choice (i.e.: this needs to be async)
+
+            return new Promise((resolve) => {
+                state.resolve = resolve
+            })
+        })
+        .then((choice) => {
+            modal.style.display = "none"
+
+            switch (choice) {
+                case "left":
+                    state.scoreLeft = state.scoreLeft + 1
+                    break
+                case "right":
+                    state.scoreRight = state.scoreRight + 1
+                    break
+                case "retry":
+                    break
+            }
+
+            roundOfCompetition()
+        })
+}
+
+function leftBtnClicked() {
+    state.resolve("left")
+}
+
+function retryBtnClicked() {
+    state.resolve("retry")
+}
+
+function rightBtnClicked() {
+    state.resolve("right")
+}
